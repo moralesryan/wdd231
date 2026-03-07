0f3b6ab7a52655863ae9caa7e165f36516023188
moralesryan <rmoralesaguilar@byupathway.edu>
2026-03-06 22:42:09 -0600

    "media query and stuff"

---
+++ code-along/scripts/navigation.js
@@ -0,0 +1,9 @@
+const navButton = document.querySelector('#ham-btn');
+const navBar = document.querySelector('#nav-bar');
+
+//toggle the show class on and off
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
+});
+
