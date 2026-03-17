f21e7ab3ced2f6b0df261c2a8482bdc2c6b9819e
moralesryan <rmoralesaguilar@byupathway.edu>
2026-03-16 21:21:50 -0600

    "done?"

---
+++ chamber/scripts/last-modified.js
@@ -0,0 +1,5 @@
+//Get dates
+const currentYear = new Date().getFullYear();
+document.getElementById("currentDates").textContent = currentYear;
+
+document.getElementById("lastModified").textContent = document.lastModified;
