74ab69a6162962099a0972cd2715ee77deffacce
moralesryan <rmoralesaguilar@byupathway.edu>
2026-03-28 21:56:26 -0600

    "fixed meta title"

--- chamber/scripts/timestamp.js
+++ chamber/scripts/timestamp.js
@@ -10,7 +10,7 @@ const modalMap = {
 };
 
 async function loadMembershipCards() {
-    const response = await fetch('data/membership.json');
+    const response = await fetch('data/memberships.json');
     const data = await response.json();
 
     //calling the div element to create the membership cards
@@ -28,6 +28,15 @@ async function loadMembershipCards() {
         const modalId = modalMap[membership.level];
         const modal = document.getElementById(modalId);
 
+        const ul = document.createElement('ul');
+        membership.details.forEach(detail => {
+            const li = document.createElement('li');
+            li.textContent = detail;
+            ul.appendChild(li);
+        });
+
+        modal.insertBefore(ul, modal.querySelector('button'));
+
         card.querySelector('.info-link').addEventListener('click', e => {
             e.preventDefault();
             modal.showModal();
@@ -39,59 +48,3 @@ async function loadMembershipCards() {
 }
 
 loadMembershipCards();
-
-
-
-
-//NP membership Modal
-/*  const nplvl = document.getElementById('np-memb');
-   const openNp = document.getElementById('openNp');
-   const closeNp = document.getElementById('closeNp');
-
-   openNp.addEventListener('click', () => {
-       nplvl.showModal();
-   });
-
-   closeNp.addEventListener('click', () => {
-       nplvl.close();
-   });
-
-   //Bronze membership Modal
-   const bronzelvl = document.getElementById('bronze-memb');
-   const openBronze = document.getElementById('openBronze');
-   const closeBronze = document.getElementById('closeBronze');
-
-   openBronze.addEventListener('click', () => {
-       bronzelvl.showModal();
-   });
-
-   closeBronze.addEventListener('click', () => {
-       bronzelvl.close();
-   });
-
-   //Silver membership Modal
-   const silverlvl = document.getElementById('silver-memb');
-   const openSilver = document.getElementById('openSilver');
-   const closeSilver = document.getElementById('closeSilver');
-
-   openSilver.addEventListener('click', () => {
-       silverlvl.showModal();
-   });
-
-   closeSilver.addEventListener('click', () => {
-       silverlvl.close();
-   });
-
-   //Gold membership Modal
-   const goldlvl = document.getElementById('gold-memb');
-   const openGold = document.getElementById('openGold');
-   const closeGold = document.getElementById('closeGold');
-
-   openGold.addEventListener('click', () => {
-       goldlvl.showModal();
-   });
-
-   closeGold.addEventListener('click', () => {
-       goldlvl.close();
-   });
-*/
